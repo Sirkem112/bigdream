672d542a06ee226aa60dae5ad7838128e73b50f2
Sirkem112 <meingithub01@gmail.com>
2022-08-31 21:48:22 +0200

    update new added files.

--- Myport/styles/app.js
+++ Myport/styles/app.js
@@ -2,3 +2,19 @@ const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.contolls');
 const sectBtn = document.querySelectorAll('.control');
 const allsections = document.querySelector('.main-content');
+
+
+
+function pageTransitions(){
+    //Button click active class
+    for (let i = 0; i < sectBtn.length; i++) {
+       sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += 'active-btn'
+        })
+        
+    }
+}
+
+pageTransitions();
