6833676c9f896bfeb10c4430cabff148df3e0b71
Sirkem112 <meingithub01@gmail.com>
2022-08-31 12:16:48 +0200

    update new changes.

---
+++ Myport/styles/app.js
@@ -0,0 +1,4 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.contolls');
+const sectBtn = document.querySelectorAll('.control');
+const allsections = document.querySelector('.main-content');
